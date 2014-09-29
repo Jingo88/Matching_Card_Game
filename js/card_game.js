e4758dd744e5eac455f2f4adb78ef8b40bc5db33
Jingo88 <jcory.ng@gmail.com>
2014-09-29 14:02:11 -0400

    Matching Card Game Edit #6
    building another array to reference. Should use it to build randomized
    cards one at a time and not double up the same shuffled array order.

--- js/card_game.js
+++ js/card_game.js
@@ -8,7 +8,23 @@ var gameWon=false;
 
 
 
+/* data types
+	string = 'this is a string'
+	integer = 1, 2, 3
+	floats = 1.1, 2.2
+	arrays = [1,2,3]
+	objects = {a: 1, b: 2}
+	booleans = true, false
+	functions = function() {}
+	undefined
+	NaN
 
+
+	1 == "1" //true
+	1 === "1" //false
+	0 === false //false
+	0 == false //true
+*/
 
 
 $(document).ready(function(){
@@ -34,45 +50,83 @@ Array.prototype.shuffle = function() {
 	return this;
 }
 
-// var cardMatches = ['card0','card1','card2','card3','card4','card5'];
-var cardMatches = ['img/card0.jpg','img/card1.jpg','img/card2.jpg',
-				'img/card3.jpg','img/card4.jpg','img/card5.jpg',];
-var result = cardMatches.shuffle();
-console.log(result);
+//DO NOT NEED THEBELOW ARRAY
+
+//double up the array? this seems like the easier way out
+// var cardMatches = ['img/card0.jpg','img/card1.jpg','img/card2.jpg',
+// 				'img/card3.jpg','img/card4.jpg','img/card5.jpg'];
+// var result = cardMatches.shuffle();
+// console.log(result);
 //YEAH THIS FUCKING WORKS!!!!
 //lets try changing the array to be linked to the images in the img folder
 
+
+
 //can I use the splice method to double up the card count?
 
+
+//maybe use a while loop to determine there are no more than 2 of any card
+//maybe build another array with the card indexes. 
+//recursive
+
+//what if I doubled up on the imgfiles array, copy and paste to create a new array
+
+//index of
+
+
+
 var imgFiles = [];
-var imageFilename = "card";
 
 function addCards(){
-	for(i=0;i<6;i++){
-		imgFiles.push("card"+i+".jpg");
-	};
-
-	
 
 	imgFiles.shuffle();
 	console.log("image array ("+imgFiles.length+" items) /// contents= "+imgFiles.toString());
-	
 
+//this loop will add the number of cards up to "i"
 	for(i=0;i<12;i++) {
 //this is a ternary operator
 //what does the % mean in this operator?
-		var cardNumber = i < 6 ? i : i % 6;
-		var cardSet = '<div class="card-container"><div class="card" data-which=""'+i+'"><div class="front"></div><div class="back"><img src="'+'img/'+imgFiles[cardNumber]+'"></div></div></div>';
+//the % gives you the remainder after a divided equation
+		//var cardNumber = i < 6 ? i : i % 6;
+		
+		var cardNumber = randomizeCard();
+		//buildingArr.push(cardNumber);
+//will be the result of the number of the function you just wrote at the bottom
+
+
+		var cardSet = '<div class="card-container"><div class="card"><div class="front"></div><div class="back"><img src="'+'img/card'+cardNumber+'.jpg"></div></div></div>';
+		
 		$('.wholegame').append(cardSet);
 		console.log("HTML appended: "+cardSet);
 	}	
 }
 
+
+//you can use this to keep the cards flipped for a certain amount of time
+
+// setInterval(onTimerTick, 33); // 33 milliseconds = ~ 30 frames per sec
+
+// function onTimerTick() {
+//     // Do stuff.
+// }
+
+function cardFlip(){
+	$('.card').click(function(){
+		cardClicks++;
+		
+		if(cardClicks<3){
+			$(this).find('.front').css('content', 'none');
+			$(this).find('.back').show();
+		}
+	})
+};
+
+
 //keeping this right now, assuming I will have to do more in the future. 
 function buildCards(){
 	addCards();
+	cardFlip();
 }
-
 
 /*
 
@@ -83,3 +137,32 @@ function buildCards(){
 	Append the full html string to the dom after the loop is finished
 
 */
+
+var buildingArr = []
+console.log(buildingArr);
+
+function randomizeCard(){
+	
+	var randomNumber = Math.floor(Math.random()*6);
+
+//how do I check if the array has the number twice already?
+//use $.inArray(value,array)? 
+
+
+	if ($.inArray(randomNumber, buildingArr) > 0) {
+		randomizeCard();
+
+//(randomeNumber is in buildingArr twice than run function randomizeCard again) {
+	} else {
+		(buildingArr.push(randomNumber));
+	}
+}
+//else if it is in not in buildingArr twice than add the index to buildingArr
+
+	//shuffle array
+	//pull one index out to fill the div
+	//use math.random here with the length of the array
+	//return the index of the random generated card
+	//build something to check the new array for 
+	//if not in arrayx2 add to array else run this function again
+
