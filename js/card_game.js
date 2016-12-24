c314939231abbfc7c63eb681d8a7b40d3307f81d
Jason <Jcory.ng@gmail.com>
2016-12-24 13:45:01 -0500

    working game, styling is shit

--- js/card_game.js
+++ js/card_game.js
@@ -1,85 +1,105 @@
-var cardClicks = 0;  //prevent more than 2 clicks
-var card1=null;  //holds image of card1 selected
-var card2=null; //holds image of card2 selected
-var cardMatching = false; 
-var clickEnabled = true;   //disables mid-animation clicks
-var cardPairs = 0;	//game won when this matches maxPairs
-var gameWon=false;
-var newCardArray = [];
-var cardMarkup = '';
+(function(){
+	let cardClicks = 0;  //prevent more than 2 clicks
+	let clickEnabled = true;   //disables mid-animation clicks
+	let cardPairs = 0;	//game won when this matches maxPairs
+	let gameWon=false;
 
-$(document).ready(function(){
+	// stores card mark up to be added to wholeGame innerHTML
+	let newCardArray = [];
 
-	buildCards();
+	// update userpick with card number
+	let userPick = null;
 
-});
+	let cardObj = {
+		0: 0,
+		1: 0,
+		2: 0,
+		3: 0,
+		4: 0,
+		5: 0
+	};
+	let totalCards = 0;
 
-//you can use this to keep the cards flipped for a certain amount of time
+	let wholeGame = document.querySelector('#wholeGame');
 
-// setInterval(onTimerTick, 33); // 33 milliseconds = ~ 30 frames per sec
-
-// function onTimerTick() {
-//     // Do stuff.
-// }
-
-// var imgFiles = [];
-
-function cardFlip(){
-	$('.card').click(function(){
-		cardClicks++;
-
-		if(cardClicks<3){
-			$(this).find('.front').css('content', 'none');
-			$(this).find('.back').show();
-		}
-	})
-};
-
-function addCards(){
-	
-	for(i=0;i<12;i++) {
-
-		getValidRandomNum(i);
-
-	}	
-}
- 
-function getValidRandomNum(iterationNumber){
-
-		var randomNumber = Math.floor(Math.random()*6);
+	const getImg = function(){
+		let randNum = Math.floor(Math.random()*6);
 		
-		var numOccurrences = getNumberOfOccurrences(randomNumber,newCardArray);
+		if (cardObj[randNum] < 2){
+			let imgSrc = `img/card${randNum}.jpg`;
 
-		if (numOccurrences == 2){
-			getValidRandomNum(iterationNumber);
+			let	cardMarkup = `
+				<div class="card-container">
+					<div class="card" data-name="${randNum}">
+						<div class="front">
+							<img src="img/card_back.jpg" data-name="${randNum}"/>
+						</div>
+						<div class="back">
+							<img src="${imgSrc}" data-name="${randNum}"/>
+						</div>
+					</div>
+				</div>
+			`;
+			cardObj[randNum]++;
+			totalCards++;
+			return cardMarkup;
 		} else {
-			getValidRandomNumberFinished(iterationNumber, randomNumber);
-		}
-}
-
-function getValidRandomNumberFinished(iterationNumber, randomNumber) {
-
-	cardMarkup += '<div class="card-container"><div class="card"><div class="front"></div><div class="back"><img src="'+'img/card'+randomNumber+'.jpg"></div></div></div>';
-	newCardArray.push(randomNumber);
-
-	if(iterationNumber == 11) {
-		$('.wholegame').append(cardMarkup);
-	}
-}
-
-function getNumberOfOccurrences(searchValue, searchArray){
-
-	var numOccurrences = 0;
-
-	for (var i=0; i < searchArray.length; i++) {
-		if( searchValue	== searchArray[i]) {
-			numOccurrences++; 
+			return getImg();
 		}
 	}
-	return numOccurrences;
-}
 
-function buildCards(){
-	addCards();
-	cardFlip();
-}
+	const buildCards = function(){
+		while (totalCards < 12){
+			newCardArray.push(getImg())
+		}
+		newCardArray.map((card)=>{
+			wholeGame.innerHTML += card
+		})
+	}
+
+// limit it to two clicks at a time. just in case the user clicks around quickly
+	const cardFlip = function(e){
+		// if e.target.dataset === {} then it is invalid
+		// otherwise grab e.target.dataset.name
+		let target = e.target.dataset.name
+		
+		if (target === undefined || gameWon){
+			return;
+		} 
+		console.log(e.target.dataset)
+		// path[2] targets the .card div that wraps front and back
+		let cardFlip = e.path[2]
+		// apply the flip class to that card dive
+		cardFlip.classList.toggle('flip');
+		
+		if (userPick === null){
+			userPick = target;
+		} else if (userPick === target){
+			userPick = null;
+			cardPairs++
+		} else {
+			setTimeout(()=> {
+				document.querySelector(`.flip[data-name="${userPick}"]`).classList.toggle('flip');
+				document.querySelector(`.flip[data-name="${target}"]`).classList.toggle('flip');
+				userPick = null;	
+			}, 1000)
+		}
+
+		if (cardPairs === 6){
+			alert("You Win");
+			gameWon = true
+		}
+
+
+		// document.querySelector(`.front img[data-name="${target}"]`).classList.toggle('flip');
+		// document.querySelector(`.back img[data-name="${target}"]`).classList.toggle('flip');
+		// console.log(document.querySelector(`.back img[data-name="${target}"].parentElement()`))
+		// document.querySelector("#myCard").classList.toggle("flip")
+		// console.log(el)
+		// el.src = `img/card${target}.jpg`	
+	}
+
+	buildCards()
+	wholeGame.addEventListener('click', cardFlip);
+})();
+
