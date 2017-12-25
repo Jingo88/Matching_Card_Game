c8109cc8a3ce12de053619c47d4112afe27bcc57
Jason Ng <jcory.ng@gmail.com>
2017-12-24 23:58:48 -0500

    added scss file, added start button function

--- js/card_game.js
+++ js/card_game.js
@@ -1,5 +1,7 @@
 (function(){
-	let cardClicks = 0;  //prevent more than 2 clicks
+    const startButton = document.querySelector('#start');
+    const resetButton = document.querySelector('#reset');
+    let cardClicks = 0;  //prevent more than 2 clicks
 	let clickEnabled = true;   //disables mid-animation clicks
 	let cardPairs = 0;	//game won when this matches maxPairs
 	let gameWon=false;
@@ -20,6 +22,8 @@
 	};
 	let totalCards = 0;
 
+
+
 	let wholeGame = document.querySelector('#wholeGame');
 
 	const getImg = function(){
@@ -31,11 +35,11 @@
 			let	cardMarkup = `
 				<div class="card-container">
 					<div class="card" data-name="${randNum}">
-						<div class="front">
-							<img src="img/card_back.jpg" data-name="${randNum}"/>
+						<div class="front" data-name="${randNum}">
+                            <img src="img/card_back.jpg" data-name="${randNum}"/>
 						</div>
 						<div class="back">
-							<img src="${imgSrc}" data-name="${randNum}"/>
+                            <img src="${imgSrc}" data-name="${randNum}"/>
 						</div>
 					</div>
 				</div>
@@ -49,6 +53,10 @@
 	}
 
 	const buildCards = function(){
+        while (wholeGame.firstChild) {
+            wholeGame.removeChild(wholeGame.firstChild);
+        }
+
 		while (totalCards < 12){
 			newCardArray.push(getImg())
 		}
@@ -59,14 +67,13 @@
 
 // limit it to two clicks at a time. just in case the user clicks around quickly
 	const cardFlip = function(e){
-		// if e.target.dataset === {} then it is invalid
-		// otherwise grab e.target.dataset.name
-		let target = e.target.dataset.name
-		
+
+        let target = e.target.dataset.name  
+
 		if (target === undefined || gameWon){
 			return;
 		} 
-		console.log(e.target.dataset)
+        
 		// path[2] targets the .card div that wraps front and back
 		let cardFlip = e.path[2]
 		// apply the flip class to that card dive
@@ -86,20 +93,15 @@
 		}
 
 		if (cardPairs === 6){
-			alert("You Win");
-			gameWon = true
+            gameWon = true
+            
+            setTimeout(()=>{
+                alert("You Win")
+            }, 1000);
+			
 		}
-
-
-		// document.querySelector(`.front img[data-name="${target}"]`).classList.toggle('flip');
-		// document.querySelector(`.back img[data-name="${target}"]`).classList.toggle('flip');
-		// console.log(document.querySelector(`.back img[data-name="${target}"].parentElement()`))
-		// document.querySelector("#myCard").classList.toggle("flip")
-		// console.log(el)
-		// el.src = `img/card${target}.jpg`	
 	}
-
-	buildCards()
-	wholeGame.addEventListener('click', cardFlip);
+	
+    wholeGame.addEventListener('click', cardFlip);
+    startButton.addEventListener('click', buildCards);
 })();
-
